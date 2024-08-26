05e796e0c1c2a52d89d93c40d9ac0297f7cd8a76
gbhem <gbhem@BLAKESPC>
2024-08-25 21:16:08 -0500

    Add project files.

---
+++ Exercise 4/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Asks user the length of rectangle
+Console.Write("Enter the length of the rectangle: ");
+
+// Makes the length be a number
+double length = Convert.ToDouble(Console.ReadLine());
+
+// Get width from the user
+Console.Write("Enter the width of the rectangle: ");
+
+// Makes the area be a number
+double width = Convert.ToDouble(Console.ReadLine());
+
+// Calculates the area of the rectangle
+double area = length * width;
+
+// Calculates the perimeter of the rectangle
+double perimeter = 2 * (length + width);
+
+Console.WriteLine("Results:");
+// Both of these lines shows the results for the area and perimeter
+Console.WriteLine($"Area of the rectangle: {area}");
+Console.WriteLine($"Perimeter of the rectangle: {perimeter}");
